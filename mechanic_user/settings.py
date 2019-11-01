5e503c692502a264f5f23e8e0cc23e1379f80c49
Ubuntu <ubuntu@ip-172-31-36-92.ec2.internal>
2019-11-01 15:39:45 +0000

    whitenoise setting updated

--- mechanic_user/settings.py
+++ mechanic_user/settings.py
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    )
+
 ROOT_URLCONF = 'mechanic_user.urls'
 
 TEMPLATES = [
@@ -131,6 +137,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
